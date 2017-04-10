fb0e7218e312679dba57127745b4e0cfed6ffb4c
ISteinDesign <istein@knights.ucf.edu>
2017-04-09 21:30:42 -0400

    initial
    commit

---
+++ js/carousel.js
@@ -0,0 +1,16 @@
+    $(document).ready(function(){
+      $('.slider').slider();
+    });
+
+
+// Pause slider
+$('.slider').slider('pause');
+// Start slider
+$('.slider').slider('start');
+// Next slide
+$('.slider').slider('next');
+// Previous slide
+$('.slider').slider('prev');
+
+
+
